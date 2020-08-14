cb3af37a5c47bb49a2e53a94f2e6eb4c2786095f
thecloudroot <62428182+thecloudroot@users.noreply.github.com>
2020-08-14 18:53:13 +0300

    rest file_sd adapter

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='rest_filesd',
+    version='1',
+    description='Prometheus rest to file sd yaml',
+    long_description=readme,
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(exclude=('tests'))
+)
