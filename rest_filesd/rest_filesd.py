8f06e64d3ae7f980c32f505dffa3b4f51d467e8d
Martin Yotov <myotov@coca-cola.com>
2020-08-21 19:40:43 +0300

    added function for rendering home page

--- rest_filesd/rest_filesd.py
+++ rest_filesd/rest_filesd.py
@@ -2,32 +2,27 @@
     PROMETHEUS REST SD
 """
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, render_template
 from flask_swagger import swagger
 from flask_swagger_ui import get_swaggerui_blueprint
 from yaml import Loader, load
-#from settings import SWAGGER_URL, swagger_path
 from .models import db
 from .models import Job, Target, Label
 from .log import logger
 from .utils import generate_targets_yaml
 
-
-# from rest_filesd.log import logger
-
+### Application logic
 SWAGGER_URL = '/api'  # URL for exposing Swagger UI (without trailing '/')
 API_URL = '/spec'  # API url
-#swagger_path=?
 #swagger_yml = load(open(swagger_path, 'r'), Loader=Loader)
-
 
 # Call factory function to create our blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
-#    '/api/swagger.json',
-      # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+      # Swagger UI static files mapped to {SWAGGER_URL}/dist
     API_URL,
-    config={  # Swagger UI config overrides
+    # Swagger UI config overrides
+    config={
         'app_name':"REST file service discovery",
         'info':{"title":"REST API"},
         'title':"REST API"
@@ -45,7 +40,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates")
     app.config.update(dict(
         TESTING = True
     ))
@@ -58,8 +53,8 @@ def create_app():
     # #os.path.join(app.root_path, 'flaskr.db'),
     # #     DEBUG=True,
     # #     SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/',
-    # #     USERNAME='admin',
-    # #     PASSWORD='default'
+    # #     USERNAME='marto',
+    # #     PASSWORD='marto'
     # ))
     # app.config.update(config or {})
     # app.config.from_envvar('FLASKR_SETTINGS', silent=True)
@@ -87,12 +82,27 @@ def create_app():
     # Start the app
     # app.run(debug=True, port=int(config.PORT))
     # app.run(debug=False, port=4999)
-    @app.route('/')
-    def index():
-        return "Prometheus service discovery"
+#    @app.route('/')
+#    def index():
+#        return "Prometheus service discovery"
 #    @app.route("/spec")
 #    def spec():
 #        return jsonify(swagger(app))
+
+# Create a URL route in our application for "/"
+    @app.route('/')
+    def home():
+        """
+        This function just responds to the browser ULR
+        localhost:5000/
+
+        :return:        the rendered template 'home.html'
+        """
+        return render_template('home.html')
+
+
+
+
 
     @app.route("/spec")
     def spec():
@@ -101,22 +111,7 @@ def create_app():
         swag['info']['title'] = "Prometheus REST API"
         return jsonify(swag)
 
-    #
-    #
-    # @app.route("/spec")
-    # def spec():
-    #     return jsonify(swagger(app))
-
-
-
     return app
-
-#swaggerui_blueprint = get_swaggerui_blueprint(
-#    SWAGGER_URL,
-#    '/api/swagger.json',
-#)
-
-
 
 
 def register_blueprints(app):
@@ -166,15 +161,15 @@ def sample_data(db):
     db.create_tables([Target, Job, Label])
 
     # Create sample jobs, targets and labels
-    sample_jobs = [{"name": "node", "port": "9110"},
+    sample_jobs = [{"name": "bug_me", "port": "9110"},
                    {"name": "blackbox", "port": ""},
-                   {"name": "slurm", "port": "9115"}]
+                   {"name": "blackbox1", "port": ""}]
     for sample in sample_jobs:
         Job.create(name=sample['name'], port=sample['port'])
         # print("Created job %s" % sample['name'])
 
     sample_targets = [{"host": "cola1.example.com", "job": "blackbox"},
-                      {"host": "cola2.example.com", "job": "blackbox"}]
+                      {"host": "cola2.example.com", "job": "blackbox1"}]
     for sample in sample_targets:
         Target.create(host=sample['host'], job=Job.select().where(Job.name == sample['job']).get())
         # print("Created target %s" % sample['host'])
